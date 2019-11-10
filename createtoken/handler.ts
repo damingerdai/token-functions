539ef48863961f7df87778172702188a7238c8e4
Arthur Ming <guobin.ming@pwc.com>
2019-11-10 11:45:07 +0800

    build(createtoken): finish create token api

--- createtoken/handler.ts
+++ createtoken/handler.ts
@@ -1,12 +1,39 @@
-"use strict"
+import jwt from 'jsonwebtoken';
+
+const secret = 'damingerdai';
 
 export = (event, context) => {
-    let err;
-    const result =             {
-        status: "You said: " + JSON.stringify(event.body)
-    };
-
-    context
-        .status(200)
-        .succeed(result);
+    const headers = event.headers;
+    const username = headers['username'] || headers['user'] || headers['name'];
+    const password  = headers['password'];
+    if (username && password) {
+        const token = jwt.sign({
+            username,
+            password
+        }, secret, {
+            expiresIn: '1h'
+        })
+        if (token) {
+            const response = {
+                token
+            };
+            context
+            .status(200)
+            .succeed(JSON.stringify(response));
+        } else {
+            const response = {
+                error: 'username and password is required'
+            };
+            context
+            .status(400)
+            .succeed(JSON.stringify(response));
+        }
+    } else {
+        const response = {
+            error: 'username and password is required'
+        };
+        context
+        .status(403)
+        .succeed(JSON.stringify(response));
+    }
 }
